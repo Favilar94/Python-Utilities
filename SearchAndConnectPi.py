6181d5ef09ac2a36b76709a2bede7b140e8924e7
Alejandro Favila <43449469+Favilar94@users.noreply.github.com>
2020-02-04 08:27:14 -0700

    Add files via upload
    Search all Raspberry pi connected in the local network and create a SSH connection

---
+++ SearchAndConnectPi.py
@@ -0,0 +1,42 @@
+"""
+ESP- Este codigo escanea todas las raspberry conectadas a la red y manda un archivo a cada una de ellas sobre SSH
+ENG- Scan all IP connected in the local network and detect all the Raspberry Pi in order to create a SSH connection
+"""
+import paramiko
+from scp import SCPClient
+
+import os 
+import time
+import subprocess
+import re
+
+def createSSHClient(server, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, username=user, password=password)
+    return client
+
+hostname=subprocess.check_output(['hostname']).decode("utf-8") #Local hostname 
+# print(hostname)
+
+ips = subprocess.check_output(['arp', '-a']).decode("utf-8") 
+rExpression='\d+\.\d+\.\d+\.\d+\s+b8-27-eb-\w+-\w+-\w+'  #All Raspeberry pi MAC Adress start with b8-27-eb
+raspberryConected= re.findall(rExpression, ips)
+
+for raspberry in raspberryConected:
+    ip=raspberry.split()[0]
+    try:
+        ssh = createSSHClient(ip, 'pi', 'raspberry') #change default user and pasword 
+        scp = SCPClient(ssh.get_transport())
+        scp.put("C:/Users/Alex/Desktop/YourFolder/yourFile.py", remote_path="/home/pi/Desktop/") #Change the paths as you required
+        print("Archivo mandado correctamente a modulo con IP: " + ip) #File sent correctly to module with IP:
+        # Prueba para reiniciar el modulo
+
+        stdin, stdout, stderr = ssh.exec_command("python3 /home/pi/Desktop/yourFile.py") #You can changue the command as you required 
+
+    except:
+        print("Archivo no mandado a modulo con IP: " + ip) #File not sent to module with IP: 
+    finally:
+        ssh.close()
+
